72b54208feae9a7ba1c09527695475924bf69b66
Schrammpy <david_luq@hotmail.com>
2026-07-21 20:23:36 -0300

    Ajustes en el Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -99,8 +99,8 @@ export default function Footer() {
                 rel="noopener noreferrer"
                 className="text-blue-600 hover:text-blue-800 underline decoration-blue-200 underline-offset-2 transition-colors"
               >
-                mydocs.studio@gmail.com
-              </a> Luque, Paraguay.
+                MyDocs Studio
+              </a> de Luque, Paraguay.
             </p>
         </div>
       </div>
